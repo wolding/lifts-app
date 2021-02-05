dc2954daffdc4b8a5c20d557ee5e9c2bc42b3dfe
wolding <will.olding22@gmail.com>
2021-02-05 20:37:46 +0000

    add function to order list by time

--- lifts_code.py
+++ lifts_code.py
@@ -13,19 +13,34 @@ class Job:
     def __repr__(self):
         return "Job from {origin} to {dest} at {time}".format(origin=self.origin, dest=self.destination, time=self.time)
 
+
 # add a job to the list and return update
 def add_job(name, joblist):
         time = input("What time do you need the lift?: hh:mm ")
         origin, destination = input("From: "), input("To: ")
         name = Job(time, origin, destination)
         joblist.append(name)
-        return joblist, name
+        return joblist
+
+# function to reorder jobs list by job time
+def sort_list(unsorted_list):
+    # set lowest value index = to [0]
+    for i in range(len(unsorted_list)):
+        lvi = i
+        for j in range(i+1, len(unsorted_list)):
+            # search for lowest time and swap it to index[0] on the list
+            if unsorted_list[j].time < unsorted_list[lvi].time:
+                lvi = j
+        unsorted_list[i], unsorted_list[lvi] = unsorted_list[lvi], unsorted_list[i]
+    return unsorted_list
+
+
 
 
 # create an empty list to hold the jobs in
 job_list = []
-
-
-job_list, name = add_job("Will", job_list)
-print(job_list, name, name.)
+job_list= add_job("Will", job_list)
+job_list= add_job("Kev", job_list)
+print(sort_list(job_list))
+print(job_list)
 
